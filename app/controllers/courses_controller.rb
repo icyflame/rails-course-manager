b164181f45ef51328caae85f6305317668111398
icyflame <kannan.siddharth12@gmail.com>
2014-09-18 17:19:06 +0530

    Initial commit. Added all the files, configured sass, through the asset pipeline.
    using it to make all the input elements have form-control.

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,32 @@
+class CoursesController < ApplicationController
+  def index
+  	@courses = Course.all
+  end
+
+  def new
+  end
+
+  def create
+  	@newcourse = Course.new(course_params)
+
+  	if @newcourse.save
+      index
+    else
+      render 'new'
+    end
+    
+  end
+
+  def show
+
+    @course = Course.find(params[:id])
+
+  end
+
+  private
+
+  def course_params
+   params.require(:courses).permit(:code, :name, :dept)
+ end
+
+end
---
+++ app/models/course.rb
@@ -0,0 +1,6 @@
+class Course < ActiveRecord::Base
+	validates :code, presence: true,
+                    length: { is: 7 }
+    validates :dept, presence: true,
+    				length: { is: 2 }
+end
